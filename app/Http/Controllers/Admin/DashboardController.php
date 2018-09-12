3a17233f9acbc29a182cf4acfa8d190ca572b5e9
e.kerimov <e.kerimov@els24.com>
2018-09-12 17:55:25 +0300

    Fixed dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Article;
+use App\Category;
 
 class DashboardController extends Controller
 {
@@ -11,6 +13,11 @@ class DashboardController extends Controller
 
     public function dashboard()
     {
-        return view('admin.dashboard');
+        return view('admin.dashboard', [
+            'categories' => Category::lastCategories(5),
+            'articles' => Article::lastArticles(5),
+            'count_categories' => Category::count(),
+            'count_articles' => Article::count(),
+        ]);
     }
 }
